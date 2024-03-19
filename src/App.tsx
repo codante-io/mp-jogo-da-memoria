05e0ef37268fac71dab4922a61b945ed20d0eb6b
jonathankarlinski <jonathankarlinski57@gmail.com>
2024-03-19 17:51:15 -0300

    feat: Criando page de game e a sua rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Game from './pages/Game';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
+      <Route path="/game" Component={Game} />
       <Route
         path="/"
         Component={Login}
