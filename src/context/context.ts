c11b075018653ebb59405081b3c713aa33a55c6e
jonathankarlinski <jonathankarlinski57@gmail.com>
2024-03-19 17:43:26 -0300

    feat: Criando componente header e atualizando context com timer

--- src/context/context.ts
+++ src/context/context.ts
@@ -3,10 +3,16 @@ import { createContext } from 'react';
 interface InitialState {
   userName: string;
   setUserName: React.Dispatch<React.SetStateAction<string>>;
+  timer: number;
+  setTimer: React.Dispatch<React.SetStateAction<number>>;
+  flag: boolean;
+  setFlag: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const INITIAL_STATE = {
   userName: '',
+  timer: 0,
+  flag: false,
 } as InitialState;
 
 const context = createContext(INITIAL_STATE);
