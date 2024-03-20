65f3df75ef277d124a707cafd885e9e38c05f203
jonathankarlinski <jonathankarlinski57@gmail.com>
2024-03-20 10:54:34 -0300

    fix: Alterando BrowserRouter para HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import Provider from './context/ContextProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 );
